48a750e3d58791bdc16db958f196b9f734515188
Regina lacerda <lacerdapinheiro@gmail.com>
2021-02-23 13:10:06 -0300

    create db

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,18 @@
+import "reflect-metadata";
 import express from 'express';
+import "./database";
 
 const app = express();
 
+
+
 app.get("/", (req, res)=>{
     return res.json({message:'Hello world - NLW4'});
+});
+
+app.post("/", (req,res)=>{
+    return res.json({message:"Os dados foram salvos com sucesso!"});
+
 });
 
 app.listen(3333, ()=>console.log('Server is running! porta 3333'))
