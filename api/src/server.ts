1a1b9a02b460dcfc223e2e0ab708145d0c938bd0
Regina lacerda <lacerdapinheiro@gmail.com>
2021-02-23 16:37:42 -0300

    create migrations

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,12 @@
 import "reflect-metadata";
 import express from 'express';
 import "./database";
+import{router} from "./routes"
 
 const app = express();
 
+app.use(express.json());
+app.use(router);
 
-
-app.get("/", (req, res)=>{
-    return res.json({message:'Hello world - NLW4'});
-});
-
-app.post("/", (req,res)=>{
-    return res.json({message:"Os dados foram salvos com sucesso!"});
-
-});
 
 app.listen(3333, ()=>console.log('Server is running! porta 3333'))
